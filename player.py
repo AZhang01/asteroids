92fe827286db509005165814bafc5181de5ff0af
AZhang01 <andyzhangten@gmail.com>
2025-07-30 20:37:01 -0700

    creating groups

--- player.py
+++ player.py
@@ -1,39 +1,39 @@
 import pygame
 from constants import *
-from circleshape import CircleShape
+from player import Player
 
 
-class Player(CircleShape):
-    def __init__(self, x, y):
-        super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-        return [a, b, c]
+    Player.containers = (updatable, drawable)
 
-    def update(self, dt):
-        keys = pygame.key.get_pressed()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-        if keys[pygame.K_w]:
-            self.move(dt)
-        if keys[pygame.K_s]:
-            self.move(-dt)
-        if keys[pygame.K_a]:
-            self.rotate(-dt)
-        if keys[pygame.K_d]:
-            self.rotate(dt)
+    dt = 0
 
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
